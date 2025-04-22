ca1f3d7e619b06b01a2a12f8f511cdc247422a71
PhamQuocLan <hao12398700@gmail.com>
2025-04-22 14:08:04 +0700

    jar8033

--- attendance-system/src/main/java/com/stu/attendance/exception/UnauthorizedException.java
+++
@@ -1,7 +0,0 @@
-package com.stu.attendance.exception;
-
-public class UnauthorizedException extends RuntimeException {
-    public UnauthorizedException(String message) {
-        super(message);
-    }
-}
--- attendance-system/src/main/java/com/stu/attendance/repository/NguoiThamGiaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.stu.attendance.repository;
-
-import com.stu.attendance.entity.NguoiThamGia;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NguoiThamGiaRepository extends JpaRepository<NguoiThamGia, Long> {
-
-}
